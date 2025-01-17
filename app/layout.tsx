32ebfc5d542b0999f888e2fbd1e9e6796b21dbf2
mooyeon-seo <seomooyeon@gmail.com>
2025-01-17 09:27:04 +0900

    Add Toaster components to RootLayout for notifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster />
+        <Sonner />
         {children}
       </body>
     </html>
